de3cb7ce6b40edd9a628a8163bce72f877f17372
IsraelNightray <israel_nightray@ciencias.unam.mx>
2021-10-14 18:15:53 -0500

    Correccion del codigo

--- Comparacion.java
+++ Comparacion.java
@@ -1,32 +1,34 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Comparacion {
     BufferedReader lector = null;
-    String linea=null;
+    String linea = null;
 
-    try
-    {
-        lector = new BufferedReader(new FileReader("resultados.txt"));
-        do {
-            linea = lector.readLine();
-            System.out.println(linea + "\n");
-            System.out.println();
-        } while (linea != null);
-    }catch(
-    FileNotFoundException e)
-    {
-        System.out.println(e);
-    }catch(
-    IOException e)
-    {
-        System.out.println(e);
-    }finally
-    {
-        try {
-            lector.close();
-        } catch (IOException e) {
+    public void leer(){
+        try
+        {
+            lector = new BufferedReader(new FileReader("resultados.txt"));
+            do {
+                linea = lector.readLine();
+                System.out.println(linea + "\n");
+                System.out.println();
+            } while (linea != null);
+        }catch(
+        FileNotFoundException e)
+        {
             System.out.println(e);
+        }catch(
+        IOException i)
+        {
+            System.out.println(i);
+        }finally
+        {
+            try {
+                lector.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
         }
     }
-}}
+
+}
