c8f747462ca3d77f3c595cd03ce7342f7ec2848f
MaiteDT <53274538+MaiteDT@users.noreply.github.com>
2021-10-14 20:58:23 -0500

    Reconoce bandera y nombre del txt

--- Comparacion.java
+++ Comparacion.java
@@ -1,62 +1,38 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Scanner;
 
-//**Clase de comparacion de computadoras por medio de sus pruebas*/
-public class Comparacion implements Interface {
-
-    //Lector del txt
-    BufferedReader lector = null;
-
-    //Linea actual que se esta leyendo (Auxiliar)
-    String lineaActual = "";
-
-    //Se guarda en cada posicion numero de computadoras a comparar y numero de resultados
-    ArrayList<Double> filaUno = new ArrayList<Double>();
-
-    //Numero de pesos
-    ArrayList<Double> filaDos = new ArrayList<Double>();
-
-    //Lista de cada renglon de resultados 
-    ArrayList<ArrayList<Double>> filaTres = new ArrayList<ArrayList<Double>>();
-    
-    //Ayuda a leer y pasar los resultados a las listas (Aun no esta hecho)
-    private void leer(){
-        try
-        {
-            lector = new BufferedReader(new FileReader("resultados.txt"));
-            do {
-                lineaActual = lector.readLine();
-                System.out.println(lineaActual + "\n");
-                System.out.println();
-            } while (lineaActual != null);
-        }catch(
-        FileNotFoundException e)
-        {
-            System.out.println(e);
-        }catch(
-        IOException i)
-        {
-            System.out.println(i);
-        }finally
-        {
+public class Comparacion {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            //El primer argumento lo guarda(que sería la bandera)
+            String bandera=new String();
+            bandera= args[0];
+            //Guarda segundo argumento (que sería el nombre del texto)
+            String texto= new String();
+            texto= args[1];
+            BufferedReader lector = null;
+            String linea = null;
+            
             try {
-                lector.close();
+                lector = new BufferedReader(new FileReader(texto));
+                do {
+                    linea = lector.readLine();
+                    System.out.println(linea + "\n");
+                } while (linea != null);
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
             } catch (IOException e) {
                 System.out.println(e);
+            } finally {
+                try {
+                    lector.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
             }
+        } else {
+            System.out.println("No se dieron argumentos para trabajar");
         }
+        
     }
-
-    public void daMejor(String banderaA, String texto){
-
-    }
-
-    public void daGeometrica(){
-
-    }
-
-    public void daPonderada(){
-
-    }
-
 }
