bb692f08f77304e967cf1d9e5d43cf9852ecd93f
IsraelNightray <israel_nightray@ciencias.unam.mx>
2021-10-17 13:47:47 -0500

    Se añadio lista de resultados y switch para cada caso de bandera

--- Comparacion.java
+++ Comparacion.java
@@ -1,7 +1,12 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Comparacion {
     public static void main(String[] args) {
+
+        // Valores del texto
+        ArrayList<String[]> valores = new ArrayList<String[]>();
+
         if (args.length > 0) {
 
             // El primer argumento lo guarda(que sería la bandera)
@@ -12,16 +17,17 @@ public class Comparacion {
             String texto = new String();
             texto = args[1];
 
-            //Buffer lector
+            // Buffer lector
             BufferedReader lector = null;
             String linea = null;
 
+            // Lectura del texto
             try {
                 lector = new BufferedReader(new FileReader(texto));
                 do {
                     linea = lector.readLine();
                     if (linea != null) {
-                        System.out.println(linea +"\n");
+                        valores.add(linea.split(" "));
                     }
                 } while (linea != null);
             } catch (FileNotFoundException e) {
@@ -35,9 +41,29 @@ public class Comparacion {
                     System.out.println(e);
                 }
             }
+
+            switch (bandera) {
+
+                case "-g":
+                    break;
+
+                case "-p":
+                    break;
+
+                case "-r":
+                    break;
+
+                case "-t":
+                    break;
+
+                default:
+                    System.out.println("No se ingreso una bandera valida ):");
+
+            }
+
         } else {
             System.out.println("No se dieron argumentos para trabajar");
-        }
+        } // Fin de lectura.
 
     }
 }
