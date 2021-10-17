da9abc13e67c4683d583b6600803a8525748da1b
IsraelNightray <israel_nightray@ciencias.unam.mx>
2021-10-17 13:13:09 -0500

    Se quito archivos basura y se modifico lectura

--- Comparacion.java
+++ Comparacion.java
@@ -1,23 +1,28 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Comparacion {
     public static void main(String[] args) {
         if (args.length > 0) {
-            //El primer argumento lo guarda(que sería la bandera)
-            String bandera=new String();
-            bandera= args[0];
-            //Guarda segundo argumento (que sería el nombre del texto)
-            String texto= new String();
-            texto= args[1];
+
+            // El primer argumento lo guarda(que sería la bandera)
+            String bandera = new String();
+            bandera = args[0];
+
+            // Guarda segundo argumento (que sería el nombre del texto)
+            String texto = new String();
+            texto = args[1];
+
+            //Buffer lector
             BufferedReader lector = null;
             String linea = null;
-            
+
             try {
                 lector = new BufferedReader(new FileReader(texto));
                 do {
                     linea = lector.readLine();
-                    System.out.println(linea + "\n");
+                    if (linea != null) {
+                        System.out.println(linea +"\n");
+                    }
                 } while (linea != null);
             } catch (FileNotFoundException e) {
                 System.out.println(e);
@@ -33,6 +38,6 @@ public class Comparacion {
         } else {
             System.out.println("No se dieron argumentos para trabajar");
         }
-        
+
     }
 }
