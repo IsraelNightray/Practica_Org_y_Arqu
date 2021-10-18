113aa196ac0d15d22b664b59ba7836fbcea3398c
MaiteDT <53274538+MaiteDT@users.noreply.github.com>
2021-10-17 21:34:32 -0500

    Funciona la ponderada

--- Comparacion.java
+++ Comparacion.java
@@ -9,18 +9,22 @@ public class Comparacion {
 
         if (args.length > 0) {
 
-            // El primer argumento lo guarda(que sería la bandera)
-            String bandera = new String();
-            bandera = args[0];
+            // El primer argumento lo guarda(que sería el calculo)
+            String calculo = new String();
+            calculo = args[0];
 
             // Guarda segundo argumento (que sería el nombre del texto)
+            String medida = new String();
+            medida = args[1];
+
             String texto = new String();
-            texto = args[1];
+            texto = args[2];
 
             // Buffer lector
             BufferedReader lector = null;
             String linea = null;
 
+            ArrayList<Double> mediasPonderadas = new ArrayList<Double>();
             // Lectura del texto
             try {
                 lector = new BufferedReader(new FileReader(texto));
@@ -31,29 +35,60 @@ public class Comparacion {
                     }
                 } while (linea != null);
             } catch (FileNotFoundException e) {
-                System.out.println(e);
+                System.out.println(e.getMessage());
             } catch (IOException e) {
-                System.out.println(e);
+                System.out.println(e.getMessage());
             } finally {
                 try {
                     lector.close();
-                } catch (IOException e) {
-                    System.out.println(e);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
             }
 
-            switch (bandera) {
+            System.out.println(calculo);
+            switch (calculo) {
 
                 case "-g":
+                    System.out.println("Aquí haríamos la geo");
                     break;
 
                 case "-p":
-                    break;
 
-                case "-r":
-                    break;
-
-                case "-t":
+                    Double ponderada = 0.0;
+                    Double pesosSumados = 0.0;
+                    for (int k = 0; k < valores.get(1).length; k++) {
+                        pesosSumados = pesosSumados + Double.parseDouble(valores.get(1)[k]);
+                    }
+                    // Calcula las medias ponderadas de cada computadora y las guarda en una lista
+                    for (int j = 2; j < valores.size(); j++) {
+                        for (int i = 0; i < valores.get(2).length; i++) {
+                            ponderada += (Double.parseDouble(valores.get(1)[i])
+                                    * Double.parseDouble(valores.get(j)[i]));
+                        }
+                        mediasPonderadas.add(ponderada / pesosSumados);
+                        ponderada = 0.0;
+                    }
+                    // Busca max y min en la lista generada anteriormente
+                    Double max = 0.0;
+                    Double min = 0.0;
+                    for (Double media : mediasPonderadas) {
+                        if (media > max) {
+                            max = media;
+                        }
+                    }
+                    min = max;
+                    for (Double media : mediasPonderadas) {
+                        if (media < min) {
+                            min = media;
+                        }
+                    }
+                    //Dependiendo de la bandera, regresa la PC 
+                    if (medida.equals("-t")) {
+                        System.out.println("PC" + mediasPonderadas.indexOf(min));
+                    } else if (medida.equals("-r")) {
+                        System.out.println("PC" + mediasPonderadas.indexOf(max));
+                    }
                     break;
 
                 default:
